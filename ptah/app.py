b355ce364634b4b522b6a9397b5c04e5a412d52d
Varkal <moreau.romain83@gmail.com>
2019-11-24 19:48:33 +0100

    :fire: Remove unnecessary test command

--- ptah/app.py
+++ ptah/app.py
@@ -2,7 +2,6 @@ from chuda import App, autorun
 from os import getenv
 
 from .commands.create import CreateCommand
-from .commands.test import TestCommand
 
 @autorun()
 class PtahApp(App):
@@ -10,4 +9,4 @@ class PtahApp(App):
     config_path = [f"{getenv('HOME')}/.ptahrc"]
     config_parser = "yaml"
 
-    subcommands = [CreateCommand, TestCommand]
+    subcommands = [CreateCommand]
